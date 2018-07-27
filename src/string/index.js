f60df8bcb7b98e6f1178c4e93d76e72becab8efc
Sasaya <storyn26383@gmail.com>
2018-07-27 15:57:31 +0800

    new string helpers

--- src/string/index.js
+++ src/string/index.js
@@ -12,4 +12,16 @@
   String.prototype.slugify = function () {
     return this.toLowerCase().replace(/[:/.?=&\s]/g, '-').replace(/-+/g, '-').replace(/^-+|-+$/g, '')
   }
+
+  String.prototype.stripTags = function () {
+    return this.replace(/<\/?[a-z0-9]+.*?>/ig, '')
+  }
+
+  String.prototype.limit = function (length, suffix = '...') {
+    return this.slice(0, length) + suffix
+  }
+
+  String.prototype.nl2br = function () {
+    return this.replace(/\r\n|\n\r|\n|\r/g, '<br>')
+  }
 })()
--- src/string/index.spec.js
+++ src/string/index.spec.js
@@ -18,4 +18,34 @@ describe('String', () => {
       expect(' A:B/C.D??E=F&G '.slugify()).toBe('a-b-c-d-e-f-g')
     })
   })
+
+  describe('#stripTags()', () => {
+    it('should strip tags', () => {
+      expect('<h1>Hello World</h1>'.stripTags()).toBe('Hello World')
+      expect('<H1>Hello World</H1>'.stripTags()).toBe('Hello World')
+      expect('<h1 class="h1">Hello World</h1>'.stripTags()).toBe('Hello World')
+      expect('<h1 class="h1">Hello < World ></h1>'.stripTags()).toBe('Hello < World >')
+    })
+  })
+
+  describe('#limit()', () => {
+    it('should truncate the given string at the specified length', () => {
+      expect('Hello World'.limit(5)).toBe('Hello...')
+      expect('您好世界您好世界'.limit(4)).toBe('您好世界...')
+    })
+
+    it('should truncate the given string with specify suffix', () => {
+      expect('Hello World'.limit(5, '***')).toBe('Hello***')
+      expect('您好世界您好世界'.limit(4, '***')).toBe('您好世界***')
+    })
+  })
+
+  describe('#nl2br()', () => {
+    it('should nl2br a string', () => {
+      expect('a\nb\nc'.nl2br()).toBe('a<br>b<br>c')
+      expect('a\rb\rc'.nl2br()).toBe('a<br>b<br>c')
+      expect('a\n\rb\n\rc'.nl2br()).toBe('a<br>b<br>c')
+      expect('a\r\nb\r\nc'.nl2br()).toBe('a<br>b<br>c')
+    })
+  })
 })
